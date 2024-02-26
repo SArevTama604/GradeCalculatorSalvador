c9102887a3131303f09b693167859eac8d913aa0
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-25 18:48:41 -0800

    Update Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the student's score:");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            char grade;
+            if (score >= 90)
+            {
+                grade = 'A';
+            }
+            else if (score >= 80)
+            {
+                grade = 'C';
+            }
+            else if (score >= 60)
+            {
+                grade = 'D';
+            }
+            else
+            {
+                grade = 'F';
+            }
+            Console.WriteLine("The student's letter grade is: " + grade);
         }
     }
 }
